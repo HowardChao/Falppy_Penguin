5e471b87def955eba0698d2564639f69d859960a
Kuan-Hao,Chao <ntueeb05howard@gmail.com>
2017-10-27 08:36:10 +0800

    Initialize the file

---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  			// to import this to get the scene
+using UnityEngine.UI; 							// allow us to create the UI object
+
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl instance;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+	public Text scoreText;
+
+	private int score = 0;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (instance);//gameObject?
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOver == true && Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	public void BirdDied(){
+		gameOverText.SetActive (true);
+		gameOver = true;
+	}
+	public void birdScored(){
+		if (gameOver) {
+			return;
+		} else {
+			score++;
+			scoreText.text = "Score: " + score.ToString ();
+		}
+	}
+
+}
---
+++ Assets/Script/RepeatingBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+
+public class RepeatingBackground : MonoBehaviour {
+
+	private BoxCollider2D groundCollider;
+	private float groundHorizontalLength;	// store the length
+	// Use this for initialization
+	void Start () {
+		groundCollider = GetComponent <BoxCollider2D> ();
+		groundHorizontalLength = groundCollider.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -groundHorizontalLength) {
+			ReadPositionBackground ();
+		}
+	}
+
+	private void ReadPositionBackground(){
+		Vector2 groundOffset = new Vector2(17.21f*2f, 0);
+		transform.position = (Vector2) transform.position + groundOffset;
+	}
+}
